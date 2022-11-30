da9656da563e00f2644856ffe333199696f78c3d
Venkat Chandra <95768120+yvsc369@users.noreply.github.com>
2022-11-30 13:46:43 +0530

    Create vpc.sh

---
+++ VPC/vpc.sh
@@ -0,0 +1,10 @@
+#gcloud commands to create network 
+gcloud compute networks create my-vpc-network --subnet-mode=custom
+
+
+# To create a subnet to already created network
+gcloud compute networks subnets create subnet-a  --network=my-vpc-network --range=10.0.1.0/24 --region=asia-southeast1
+gcloud compute networks subnets create subnet-b  --network=my-vpc-network --range=10.0.2.0/24 --region=asia-southeast1
+
+gcloud compute instances create instance-1a --zone=asia-southeast1-a --machine-type=f1-micro --subnet=subnet-a 
+gcloud compute instances create instance-1b --zone=asia-southeast1-a --machine-type=f1-micro --subnet=subnet-a --no-address
