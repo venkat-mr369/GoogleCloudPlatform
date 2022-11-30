b362c1eaee296e0fc25ebcd6882f6e2a5d769553
Venkat Chandra <95768120+yvsc369@users.noreply.github.com>
2022-11-30 11:16:39 +0530

    Update vpcpeering.sh

--- VPC/vpcpeering.sh
+++ VPC/vpcpeering.sh
@@ -11,25 +11,20 @@
 gcloud compute networks create my-network-1 --subnet-mode=custom 
 
 gcloud compute networks subnets create subnet-1a --network=my-network-1 --region=europe-west1 --range=10.0.1.0/24
-
 gcloud compute networks subnets create subnet-1b --network=my-network-1 --region=europe-west1 --range=10.1.1.0/24
 
 
 gcloud compute networks create my-network-2 --subnet-mode=custom
 
 gcloud compute networks subnets create subnet-2a --network=my-network-2 --region=europe-west1 --range=10.0.2.0/24
-
 gcloud compute networks subnets create subnet-2b --network=my-network-2 --region=europe-west1 --range=10.1.2.0/24
-
-gcloud compute networks subnets create conflict-with-my-network-1-subnet --network=my-network-2 --region=europe-west1 --range=10.0.1.0/24
+gcloud compute networks subnets create conflict-with-my-network-2-subnet --network=my-network-2 --region=europe-west1 --range=10.0.1.0/24
 
 gcloud compute networks create my-network-3 --subnet-mode=custom
 
 gcloud compute networks subnets create subnet-3a --network=my-network-3 --region=europe-west1 --range=10.0.3.0/24
-
 gcloud compute networks subnets create subnet-3b --network=my-network-3 --region=europe-west1 --range=10.1.3.0/24
-
-gcloud compute networks subnets create conflict-with-network-2-subnet --network=my-network-3 --region=europe-west1 --range=10.0.2.0/24
+gcloud compute networks subnets create conflict-with-my-network-3-subnet --network=my-network-3 --region=europe-west1 --range=10.0.2.0/24
 
 echo "Create firewall rules to allow port 22 , icmp access for all network resources"
 
