24e71cef15dc8c3b992eec8ea6fee156e70a59f1
Venkat Chandra <95768120+yvsc369@users.noreply.github.com>
2022-11-30 09:13:05 +0530

    Update vpcpeering.sh

--- VPC/vpcpeering.sh
+++ VPC/vpcpeering.sh
@@ -4,20 +4,18 @@
 #echo Delete default VPC
 
 #gcloud compute firewall-rules delete default-allow-icmp default-allow-internal default-allow-rdp default-allow-ssh --quiet
-
 #gcloud compute networks delete default --quiet
-
 #echo Create 3 VPC networks, with purposely overlapping subnets
 
 
-gcloud compute networks create network-1 --subnet-mode=custom 
+gcloud compute networks create my-network-1 --subnet-mode=custom 
 
 gcloud compute networks subnets create subnet-1a --network=my-network-1 --region=europe-west1 --range=10.0.1.0/24
 
 gcloud compute networks subnets create subnet-1b --network=my-network-1 --region=europe-west1 --range=10.1.1.0/24
 
 
-gcloud compute networks create network-2 --subnet-mode=custom
+gcloud compute networks create my-network-2 --subnet-mode=custom
 
 gcloud compute networks subnets create subnet-2a --network=my-network-2 --region=europe-west1 --range=10.0.2.0/24
 
@@ -25,7 +23,7 @@ gcloud compute networks subnets create subnet-2b --network=my-network-2 --region
 
 gcloud compute networks subnets create conflict-with-my-network-1-subnet --network=my-network-2 --region=europe-west1 --range=10.0.1.0/24
 
-gcloud compute networks create network-3 --subnet-mode=custom
+gcloud compute networks create my-network-3 --subnet-mode=custom
 
 gcloud compute networks subnets create subnet-3a --network=my-network-3 --region=europe-west1 --range=10.0.3.0/24
 
@@ -33,7 +31,7 @@ gcloud compute networks subnets create subnet-3b --network=my-network-3 --region
 
 gcloud compute networks subnets create conflict-with-network-2-subnet --network=my-network-3 --region=europe-west1 --range=10.0.2.0/24
 
-echo Create firewall rules to allow port 22 , icmp access for all network resources
+echo "Create firewall rules to allow port 22 , icmp access for all network resources"
 
 gcloud compute firewall-rules create ssh-allow-network-1 --direction=INGRESS --priority=1000 --network=my-network-1 --action=ALLOW --rules=tcp:22 --source-ranges=0.0.0.0/0
 gcloud compute firewall-rules create icmp-allow-network-1 --direction=INGRESS --priority=1000 --network=my-network-1 --action=ALLOW --rules=icmp --source-ranges=0.0.0.0/0
@@ -45,7 +43,7 @@ gcloud compute firewall-rules create icmp-allow-network-2 --direction=INGRESS --
 gcloud compute firewall-rules create ssh-allow-network-3 --direction=INGRESS --priority=1000 --network=my-network-3 --action=ALLOW --rules=tcp:22 --source-ranges=0.0.0.0/0
 gcloud compute firewall-rules create icmp-allow-network-3 --direction=INGRESS --priority=1000 --network=my-network-3 --action=ALLOW --rules=icmp --source-ranges=0.0.0.0/0
 
-echo Create a instance in each created subnet
+echo "Create a 3 instance's in each created subnet"
 
 gcloud compute instances create myinstance-1 --zone=europe-west1-a --machine-type=f1-micro --subnet=subnet-1a
 
