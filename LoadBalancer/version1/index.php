c65ef8764467a8cd547337e82b1d31e32be91303
Venky Mama <95768120+yvsc369@users.noreply.github.com>
2023-01-27 08:01:14 +0530

    Update index.php

--- LoadBalancer/version1/index.php
+++ LoadBalancer/version1/index.php
@@ -6,6 +6,6 @@ echo "<h2>Server Name</h2>";
 echo "Server Hostname: " . php_uname("n");
 echo "<h2>Server Location</h2>";
 echo "Region and Zone: " . "region-here";
-echo "<h1>Hello Google World - Welcome to GCP Platform </h1>";
-echo "<h2>Google Cloud is Rocking ..yo yo yo</h2>";
+echo "<h1>Hello Google World - Welcome to GCP Platform - VERSION 1</h1>";
+echo "<h2>Google Cloud is Rocking</h2>";
 ?>
