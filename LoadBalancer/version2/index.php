fb866aff14e6b7313bb81134587dfde28d3efad7
Venky Mama <95768120+yvsc369@users.noreply.github.com>
2023-01-29 11:00:56 +0530

    Update index.php

--- LoadBalancer/version2/index.php
+++ LoadBalancer/version2/index.php
@@ -1,13 +1,13 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
 // display it back
-echo "<h1>VERSION 2</h1>";
+echo "<h1>Hello Google - Welcome to Cloud Platform - VERSION 2</h1>";
+echo "<h1>Webapp Sample Page</h1>";
 echo "<h2>Server Hostname</h2>";
 echo "Server Hostname: " . php_uname("n");
 echo "<h2>Server Location</h2>";
 echo "Region and Zone: " . "region-here";
-echo "<h1>Hello Google - Welcome to Cloud Platform - VERSION 2</h1>";
 echo '<i style="color:blue;font-size:40px;font-family:calibri ;">
       Google Cloud all Services Online Training !!2</i> ';
-echo "<h2>GCP Online Training Contact:- venkat.mr369@gmail.com</h2>";
+echo "<h2>GCP Online Training Contact Us</h2>";
 ?>
