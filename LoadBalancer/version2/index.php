bf7f830ffb1567a9472fb9b4ca8b0daa809085eb
Venky Mama <95768120+yvsc369@users.noreply.github.com>
2023-01-29 09:28:33 +0530

    Update index.php

--- LoadBalancer/version2/index.php
+++ LoadBalancer/version2/index.php
@@ -7,5 +7,6 @@ echo "Server Hostname: " . php_uname("n");
 echo "<h2>Server Location</h2>";
 echo "Region and Zone: " . "region-here";
 echo "<h1>Hello Google - Welcome to Cloud Platform - VERSION 2</h1>";
+echo '<body style="background-color:Iceberg">';
 echo "<h2>Google Cloud Online Training VERSION 02</h2>";
 ?>
