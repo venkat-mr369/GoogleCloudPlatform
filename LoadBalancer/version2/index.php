23259e86d10c197b967dd2b28538f0d90e7d45fa
Venky Mama <95768120+yvsc369@users.noreply.github.com>
2023-01-29 09:56:31 +0530

    Update index.php

--- LoadBalancer/version2/index.php
+++ LoadBalancer/version2/index.php
@@ -9,5 +9,5 @@ echo "Region and Zone: " . "region-here";
 echo "<h1>Hello Google - Welcome to Cloud Platform - VERSION 2</h1>";
 echo '<i style="color:blue;font-size:40px;font-family:calibri ;">
       Google Cloud all Services Online Training !!2</i> ';
-echo "<h2>GCP Online Training Contact: venkat.mr369@gmail.com</h2>";
+echo "<h2>GCP Online Training Contact:- venkat.mr369@gmail.com</h2>";
 ?>
