2e2c0b0202cdcb712b0c48be4682e050d4b11ff5
Venky Mama <95768120+yvsc369@users.noreply.github.com>
2023-01-29 11:01:43 +0530

    Update index.php

--- LoadBalancer/version2/index.php
+++ LoadBalancer/version2/index.php
@@ -2,10 +2,10 @@
 $ip = $_SERVER['REMOTE_ADDR'];
 // display it back
 echo "<h1>Hello Google - Welcome to Cloud Platform - VERSION 2</h1>";
-echo "<h1>Webapp Sample Page</h1>";
+echo "<h2>Webapp Sample Page</h2>";
 echo "<h2>Server Hostname</h2>";
 echo "Server Hostname: " . php_uname("n");
-echo "<h2>Server Location</h2>";
+echo "<h1>Server Location</h1>";
 echo "Region and Zone: " . "region-here";
 echo '<i style="color:blue;font-size:40px;font-family:calibri ;">
       Google Cloud all Services Online Training !!2</i> ';
