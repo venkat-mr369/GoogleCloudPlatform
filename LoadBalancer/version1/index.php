ed425678c5b927eeb06d819fc87ebd01bb398a9c
Venky Mama <95768120+yvsc369@users.noreply.github.com>
2023-01-29 10:59:45 +0530

    Update index.php

--- LoadBalancer/version1/index.php
+++ LoadBalancer/version1/index.php
@@ -1,7 +1,7 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
 // display it back
-echo "<h1>VERSION 1</h1>";
+echo "<h1>Webapp Sample Page/h1>";
 echo "<h1>Hello Google World - Welcome to GCP Platform - VERSION 1</h1>";
 echo "<h2>Server Name</h2>";
 echo "Server Hostname: " . php_uname("n");
